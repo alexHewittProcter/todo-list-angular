713e97d833fd917a8c33b7fded23eebf5928032b
alexHewittProcter <alexhp@hotmail.co.uk>
2021-01-27 18:17:33 +0000

    Adding Api, DB and Docker

---
+++ api/data/mock-todos.js
@@ -0,0 +1,20 @@
+const todos = [
+  {
+    title: "test",
+    description: "Testsss",
+    status: "open",
+  },
+  {
+    title: "Make project",
+    description: "Make a serious project",
+    status: "open",
+  },
+  { title: "No such thing", description: "Project", status: "done" },
+  {
+    title: "Clear room",
+    description: "Does this need explaining",
+    status: "done",
+  },
+];
+
+module.exports = { todos };
---
+++ api/db/seeders/20201124225725-todo.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { todos } = require("../../data/mock-todos");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "todos",
+      todos.map((item) => ({
+        ...item,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }))
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("todos", null, {});
+  },
+};
