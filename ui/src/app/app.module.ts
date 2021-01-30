78ff3f7a99379188e5fb4176b3dafa690325eeaf
alexHewittProcter <alexhp@hotmail.co.uk>
2021-01-30 16:17:29 +0000

    Adding navbar

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HttpClientModule } from "@angular/common/http";
-import { StoreModule } from "@ngrx/store";
-import { reducers } from "./core/store/reducer";
-import { TodosModule } from "./todos/todos.module";
-import { EffectsModule } from "@ngrx/effects";
-import { TodosEffects } from "./core/store/effects/todos";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './core/store/reducer';
+import { TodosModule } from './todos/todos.module';
+import { EffectsModule } from '@ngrx/effects';
+import { TodosEffects } from './core/store/effects/todos';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +22,7 @@ import { environment } from '../environments/environment';
     HttpClientModule,
     NgbModule,
     StoreModule.forRoot(reducers, {}),
+    NavbarModule,
     TodosModule,
     EffectsModule.forRoot([TodosEffects]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
